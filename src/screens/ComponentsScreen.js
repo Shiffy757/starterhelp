094c1edc129f02591304f18ac050b9aee1349cf7
Shiffy757 <shiffy757@gmail.com>
2020-06-29 11:45:48 -0400

    first push

---
+++ src/screens/ComponentsScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, StyleSheet, View } from "react - native";
+
+const ComponentsScreen = () => {
+	return <Text style={styles.textStyles}>This is the components screen</Text>;
+};
+
+const styles = StyleSheet.create({
+	textStyle: {
+		fontSize: 50
+	}
+});
+
+export default ComponentsScreen;
